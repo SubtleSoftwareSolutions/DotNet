a86e683f759a0b23193cee291e0c9b26f296c679
RoughTomato <adabkowski@gmail.com>
2017-03-19 21:03:01 +0100

    WindowsFormsCalculator

---
+++ Calculator_Windows_Forms/Lista1/Form1.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Lista1
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button buttonPercent;
+        private System.Windows.Forms.Button buttonLeft;
+        private System.Windows.Forms.Button buttonRight;
+        private System.Windows.Forms.Button buttonOnePerX;
+        private System.Windows.Forms.Button buttonCE;
+        private System.Windows.Forms.Button buttonC;
+        private System.Windows.Forms.Button buttonBackspace;
+        private System.Windows.Forms.Button buttonDiv;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button buttonMultiply;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button buttonMinus;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonPlus;
+        private System.Windows.Forms.Button buttonPlusMinus;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button buttonDot;
+        private System.Windows.Forms.Button buttonEquals;
+        private System.Windows.Forms.TextBox textResult;
+    }
+}
+
---
+++ Calculator_Windows_Forms/Lista1/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lista1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox.AppendText("1");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox.AppendText("2");
+        }
+
+        private void buttonDiv_Click(object sender, EventArgs e)
+        {
+            textBox.AppendText("/");
+        }
+
+        private void buttonOnePerX_Click(object sender, EventArgs e)
+        {
+            var prev = textBox.Text;
+            textBox.Text = "";
+            textBox.AppendText("1/(" + prev + ")");
+        }
+
+        private void buttonCE_Click(object sender, EventArgs e)
+        {
+            textResult.Text = "";
+        }
+
+        private void buttonC_Click(object sender, EventArgs e)
+        {
+            textResult.Text = "";
+            textBox.Text = "";
+        }
+
+        private void buttonBackspace_Click(object sender, EventArgs e)
+        {
+            if (textBox.Text != "") {
+                textBox.Text = textBox.Text.Remove(textBox.Text.Length - 1);
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox.AppendText("7");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox.AppendText("8");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox.AppendText("9");
+        }
+
+        private void buttonMultiply_Click(object sender, EventArgs e)
+        {
+            textBox.AppendText("*");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox.AppendText("4");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox.AppendText("5");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox.AppendText("6");
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            textBox.AppendText("-");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox.AppendText("3");
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            textBox.AppendText("+");
+        }
+
+        private void buttonPlusMinus_Click(object sender, EventArgs e)
+        {
+            var prev = textBox.Text;
+            textBox.Text = "-(" + prev + ")";
+        }
+
+        private void button0_Click(object sender, EventArgs e)
+        {
+            textBox.AppendText("0");
+        }
+
+        private void buttonDot_Click(object sender, EventArgs e)
+        {
+            textBox.AppendText(".");
+        }
+
+        private void buttonEquals_Click(object sender, EventArgs e)
+        {                     
+            try
+            {
+                textResult.Text = (new DataTable().Compute(textBox.Text, null)).ToString();
+            }
+            catch (System.Data.SyntaxErrorException ex)
+            {
+                textResult.Text = "Syntax error (;__;)<";
+            }
+            finally {
+                textBox.Text = "";
+            }
+        }
+
+        private void buttonPercent_Click(object sender, EventArgs e)
+        {
+            textBox.AppendText("%");
+        }
+
+        private void buttonLeft_Click(object sender, EventArgs e)
+        {
+            textBox.AppendText("(");
+        }
+
+        private void buttonRight_Click(object sender, EventArgs e)
+        {
+            textBox.AppendText(")");
+        }
+    }
+}
